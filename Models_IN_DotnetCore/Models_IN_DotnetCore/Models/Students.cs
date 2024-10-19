2a5ce7753e2ecd3ba7bea0dc4dbf75a13e593bfc
akashmishra998427 <168962569+akashmishra998427@users.noreply.github.com>
2024-10-19 05:52:49 -0400

    Create Students.cs

---
+++ Models_IN_DotnetCore/Models_IN_DotnetCore/Models/Students.cs
@@ -0,0 +1,12 @@
+namespace Models_IN_DotnetCore.Models
+{
+    public class Student
+    {
+
+        public int RollNo { get; set; }
+        public String Name { get; set; }
+        public String Gender { get; set; }
+        public int Standerd { get; set; }
+
+    }
+}
